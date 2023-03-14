a1651bbd777a15398207318619b9fddc61cbb2d9
Ryan MacInnis <MacInnis82@gmail..com>
2023-03-14 08:24:23 -0400

    Basic structure in place

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Flex, Image } from "@chakra-ui/react";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="36px" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{
+            base: "none",
+            md: "unset",
+          }}
+        />
+      </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      <RightContent />
+    </Flex>
+  );
+};
+
+export default Navbar;
