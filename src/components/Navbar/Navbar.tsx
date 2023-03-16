f48e8928c47003b57ee1039358a3b79ad0c59d69
Ryan MacInnis <MacInnis82@gmail.com>
2023-03-16 16:35:45 -0400

    Finish Navbar and create Add Community modal

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/src/firebase/clientApp";
 import { Flex, Image } from "@chakra-ui/react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
@@ -8,8 +9,17 @@ const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
 
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+      >
         <Image src="/images/redditFace.svg" height="36px" />
         <Image
           src="/images/redditText.svg"
@@ -20,8 +30,8 @@ const Navbar: React.FC = () => {
           }}
         />
       </Flex>
-      {/* <Directory /> */}
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
   );
--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -43,14 +43,20 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
                   color="gray.300"
                   as={FaRedditSquare}
                 />
-                <Flex direction="column" display={{ base: "none", lg: "flex"}} fontSize="8pt" align="flex-start" mr={8}>
-                    <Text fontWeight={700}>
-                        {user?.displayName || user.email?.split("@")[0]}
-                    </Text>
-                    <Flex>
-                        <Icon as={IoSparkles} color="brand.100" mr={1} />
-                        <Text color="gray.400">1 karma</Text>
-                    </Flex>
+                <Flex
+                  direction="column"
+                  display={{ base: "none", lg: "flex" }}
+                  fontSize="8pt"
+                  align="flex-start"
+                  mr={8}
+                >
+                  <Text fontWeight={700}>
+                    {user?.displayName || user.email?.split("@")[0]}
+                  </Text>
+                  <Flex>
+                    <Icon as={IoSparkles} color="brand.100" mr={1} />
+                    <Text color="gray.400">1 karma</Text>
+                  </Flex>
                 </Flex>
               </>
             ) : (
